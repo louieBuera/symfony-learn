9e41247dcc8bfabf5404806fe43fd079e843d28a
louieBuera <isaac.277.elifer@gmail.com>
2025-05-17 15:01:18 +0800

    Add db entities to template

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
 
 final class ProductController extends AbstractController
 {
+
     #[Route('/product', name: 'products_index')]
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $repository->findAll()
         ]);
     }
 }
